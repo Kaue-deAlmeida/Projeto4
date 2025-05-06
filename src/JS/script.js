1d4fed300cb1727aa77e08969fb002d6e7837fdc
Kaue de Almeida <kaualmeidapena@gmail.com>
2025-05-06 09:56:13 -0300

    Adicionando Quiz

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,6 +1,23 @@
 // Declarando o array  de imagens
 let imagens=[
-    'src/assets/imagen1.jpg',
+    'src/assets/imagem1.jpg',
     'src/assets/imagem2.jpg',
-    'src/assets/iamgem3.jpg'
+    'src/assets/imagem3.jpg'
 ];
+
+// Declarando as variaveis
+
+let i=0;
+let tempo=3000;
+
+//Criando a função slide show
+
+function slideshow(){
+    document.getElementById('image').src=imagens[i];
+    i++;
+    if(i == imagens.length){
+        i=0;
+    }
+    setTimeout("slideshow()",tempo)
+}
+slideshow();
